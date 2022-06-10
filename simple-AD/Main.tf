d718d247fe4706c38c29bad71df7824e1523b118
Erik Johnson <erikj2388@gmail.com>
2022-06-10 17:57:31 -0400

    Backend settings added

--- simple-AD/Main.tf
+++ simple-AD/Main.tf
@@ -1,6 +1,23 @@
 ##########################################################
 # Configure the Azure Provider
 ##########################################################
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.10.0"
+    }
+  }
+
+  backend "azurerm" {
+        resource_group_name  = "terraformfstate"
+        storage_account_name = "tfstate821479423"
+        container_name       = "tfstate-simple-ad"
+        key                  = "terraform.tfstate"
+    }
+
+}
+
 provider "azurerm" {
   features {}
 }
