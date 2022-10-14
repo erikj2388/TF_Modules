63aa3e0c07c4b6d08fea5eeb9cf40b10e1b5c682
Erik Johnson <erikj2388@gmail.com>
2022-10-14 15:55:00 -0400

    Updated bash script for nootstrapping

--- Linux-VM/azure-user-data.sh
+++ Linux-VM/azure-user-data.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 sudo apt-get update -y
+sudo apt-get install nmap -y
 sudo apt install docker.io -y
 sudo snap install docker -y
-docker pull rustscan/rustscan:1.10.0
+docker pull rustscan/rustscan:1.10.0az showacco
